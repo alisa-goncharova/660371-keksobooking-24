e189d79dffd1f9c3c48ba9bae27c758e709fc42b
Alisa Goncharova <61043716+alisa-goncharova@users.noreply.github.com>
2021-10-12 16:02:40 +0300

    Исправление ошибок
    Исправление наименований функций, лишней вложенности

--- js/main.js
+++ js/main.js
@@ -1,22 +1,18 @@
-const randomNumberInt = (startNumber, endNumber) => {
-  if(startNumber >= 0 && endNumber >= 0){
-    if(endNumber - startNumber > 0){
-      return Math.floor(startNumber + Math.random()  * (endNumber + 1 - startNumber));
-    }
+const getRandomNumberInt = (startNumber, endNumber) => {
+  if(startNumber >= 0 && endNumber >= 0 && endNumber - startNumber > 0){
+    return Math.floor(startNumber + Math.random()  * (endNumber + 1 - startNumber));
   }
   return 'Введены не корректные значения';
 };
 
-randomNumberInt(1, 100);
+getRandomNumberInt(1, 100);
 
-const mapCoordinates = (startNumber, endNumber, count) => {
-  if(startNumber >= 0 && endNumber >= 0){
-    if(endNumber - startNumber > 0){
-      const number = startNumber - 0.5 + Math.random() * (endNumber - startNumber + 1);
-      return Number(number.toFixed(count));
-    }
+const getMapCoordinates = (startNumber, endNumber, count) => {
+  if(startNumber >= 0 && endNumber >= 0 && endNumber - startNumber > 0){
+    const number = startNumber - 0.5 + Math.random() * (endNumber - startNumber + 1);
+    return Number(number.toFixed(count));
   }
   return 'Введены не корректные значения';
 };
 
-mapCoordinates(1, 3, 5);
+getMapCoordinates(1, 3, 5);
