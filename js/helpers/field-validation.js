7447cf1cb98e1e8bf29293701d4817d9acca6606
Alisa Goncharova <61043716+alisa-goncharova@users.noreply.github.com>
2021-11-02 14:16:11 +0300

    Правка
    Исправление отправки формы при минимальной цене меньше указанной

--- js/helpers/field-validation.js
+++ js/helpers/field-validation.js
@@ -1,3 +1,5 @@
+import { getMinPrice } from './get-min-price.js';
+
 const TITLE_MIN = 30;
 const TITLE_MAX = 100;
 const PRICE_MAX = 1000000;
@@ -19,10 +21,11 @@ const correctionLayout = (title, price, isTitle, isPrice) =>{
 const fieldsValidation = (title, price) =>{
   let isTitle = false;
   let isPrice = false;
+  const MIN_PRICE = getMinPrice(price);
   if(title.value.length >= TITLE_MIN && title.value.length <= TITLE_MAX){
     isTitle = true;
   }
-  if(price.value <= PRICE_MAX && price.value.length > PRICE_LENGTH){
+  if(price.value <= PRICE_MAX && price.value.length > PRICE_LENGTH && Number(price.value) > Number(MIN_PRICE)){
     isPrice = true;
   }
   correctionLayout(title, price, isTitle, isPrice);
